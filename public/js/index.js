ee338c12b3fb2a44b391577fe1ff3dfe4a39305d
Hiep Nguyen <thienhiep1711@users.noreply.github.com>
2019-04-02 09:57:00 +0700

    message form and jquery

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,9 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('New message!', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`)
+    $('#message-list').append(li);
 })
 
 socket.emit('createMessage', {
@@ -19,4 +22,14 @@ socket.emit('createMessage', {
 
 }, function() {
     console.log("Got it")
+})
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function() {
+
+    })
 })
